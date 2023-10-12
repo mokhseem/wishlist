d7e26f71029ed60263b07a3ae068b11b1ce890db
mokhseem <mokhseem@outlook.com>
2023-10-13 00:54:43 +0300

    Colorize cards by price range

--- scripts/index.js
+++ scripts/index.js
@@ -13,24 +13,44 @@ const getTemplateById = (id) => {
   return fragment.firstElementChild;
 };
 
+const checkPriceRange = (value) => {
+  switch (true) {
+    case value >= 3500:
+      return 'card--5k';
+    case value >= 1500:
+      return 'card--2k';
+    case value >= 750:
+      return 'card--1k';
+    case value >= 350:
+      return 'card--500';
+    case value >= 150:
+      return 'card--200';
+    case value < 150:
+      return 'card--100';
+  }
+};
+
 const wishlist = document.body.querySelector('.list');
 const wishlistFragment = document.createDocumentFragment();
 const cardTemplate = getTemplateById('card');
 
 const fillOutCard = (jsonArray) => {
   if (cardTemplate) {
-    for (const {seller, image, title, description, price} of jsonArray) {
+    for (const {link, image, title, description, price} of jsonArray) {
       const cardClone = cardTemplate.cloneNode(true);
 
-      cardClone.querySelector('.seller__icon').src = seller['icon'];
-      cardClone.querySelector('.seller__icon').alt = seller['name'];
-      cardClone.querySelector('.card__image').src = image['src'];
-      cardClone.querySelector('.card__image').alt = image['alt'];
+      cardClone.querySelector('.seller__icon').src = link.primary.src;
+      cardClone.querySelector('.seller__icon').alt = link.primary.alt;
+      cardClone.querySelector('.card__image').src = image.src;
+      cardClone.querySelector('.card__image').alt = image.alt;
       cardClone.querySelector('.card__title > a').textContent = title;
       cardClone.querySelector('.card__description').textContent = description;
       cardClone.querySelector('.card__price > data').value = price;
       cardClone.querySelector('.card__price > data')
         .textContent = new Intl.NumberFormat('ru-RU').format(price);
+
+      cardClone.querySelector('.card')
+        .classList.add(checkPriceRange(price));
 
       wishlistFragment.append(cardClone);
     }
