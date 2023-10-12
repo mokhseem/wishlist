1634df29535635bf99e9aa4873d5562a79930dfb
mokhseem <mokhseem@outlook.com>
2023-10-12 05:20:58 +0300

    Templatize cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,43 @@
+// local:
+// const jsonFile = '../data/wishlist.json';
+
+// pages:
 const jsonFile = '/wishlist/data/wishlist.json';
 
-// const response = await fetch(jsonFile);
-// const data = await response.json();
+const getTemplateById = (id) => {
+  const template = document.querySelector(`#${id}`);
+  if (template === null) {
+    return template;
+  }
+  const fragment = template.content;
+  return fragment.firstElementChild;
+};
+
+const wishlist = document.body.querySelector('.list');
+const wishlistFragment = document.createDocumentFragment();
+const cardTemplate = getTemplateById('card');
+
+const fillOutCard = (jsonArray) => {
+  if (cardTemplate) {
+    for (const {seller, image, title, description, price} of jsonArray) {
+      const cardClone = cardTemplate.cloneNode(true);
+
+      cardClone.querySelector('.seller__icon').src = seller['icon'];
+      cardClone.querySelector('.seller__icon').alt = seller['name'];
+      cardClone.querySelector('.card__image').src = image['src'];
+      cardClone.querySelector('.card__image').alt = image['alt'];
+      cardClone.querySelector('.card__title > a').textContent = title;
+      cardClone.querySelector('.card__description').textContent = description;
+      cardClone.querySelector('.card__price > data').value = price;
+      cardClone.querySelector('.card__price > data')
+        .textContent = new Intl.NumberFormat('ru-RU').format(price);
+
+      wishlistFragment.append(cardClone);
+    }
+    wishlist.append(wishlistFragment);
+  }
+};
 
 fetch(jsonFile)
   .then((response) => response.json())
   .then((data) => fillOutCard(data));
-
-// const xhr = new XMLHttpRequest();
-// xhr.open('GET', jsonFile);
-// xhr.responseType = 'json';
-// xhr.onload = () => {
-//   const data = xhr.response;
-//   fillOutCard(data);
-// };
-// xhr.send();
-
-const card = document.body.querySelector('.card');
-const seller = card.querySelector('.seller__icon');
-const image = card.querySelector('.card__image');
-const title = card.querySelector('.card__title');
-const description = card.querySelector('.card__description');
-const price = card.querySelector('.card__price > data');
-
-function fillOutCard(jsonObj) {
-  seller.src = jsonObj['seller'];
-  image.src = jsonObj['image'];
-  title.textContent = jsonObj['title'];
-  description.textContent = jsonObj['description'];
-  price.value = jsonObj['price'];
-  price.textContent = new Intl.NumberFormat('ru-RU').format(price.value);
-}
